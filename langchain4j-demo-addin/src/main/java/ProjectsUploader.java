1cb60e6086f8837832c661027dcab605df3f33a2
Serdar <serdar.basegmez@gmail.com>
2024-04-14 13:09:58 +0100

    Added java addin module and bumped up the JNX version to a local build.

---
+++ langchain4j-demo-addin/src/main/java/ProjectsUploader.java
@@ -0,0 +1,44 @@
+import com.developi.engage24.QDrantUploader;
+import com.developi.engage24.data.EmbeddingSource;
+import com.developi.engage24.data.ModelType;
+import com.hcl.domino.DominoClient;
+import com.hcl.domino.exception.ObjectDisposedException;
+import com.hcl.domino.exception.QuitPendingException;
+import com.hcl.domino.mq.MessageQueue;
+import com.hcl.domino.server.RunJavaAddin;
+import com.hcl.domino.server.ServerStatusLine;
+import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
+
+public class ProjectsUploader extends RunJavaAddin {
+
+
+    public ProjectsUploader() {
+        super("Engage24AddIn");
+    }
+
+    @Override
+    protected void runAddin(final DominoClient client, final ServerStatusLine statusLine, final MessageQueue queue) {
+        System.out.println("ProjectsUploader is running");
+        statusLine.setLine("Engage 2024 Projects Uploader");
+
+        try {
+            QDrantUploader uploader = new QDrantUploader(EmbeddingSource.PROJECTS, ModelType.LOCAL_MINILM, new AllMiniLmL6V2EmbeddingModel());
+
+            uploader.setMessageConsumer(System.out::println);
+
+            uploader.setCounterConsumer(count -> {
+                if (count % 50 == 0) {
+                    statusLine.setLine("Processed " + count + " documents");
+                }
+            });
+
+            uploader.run(client);
+        } catch (QuitPendingException | ObjectDisposedException e) {
+            System.out.println("ProjectsUploader was interrupted! Exiting...");
+        }
+    }
+
+    public synchronized void stopAddin() {
+        System.out.println("Stopping...");
+    }
+}
